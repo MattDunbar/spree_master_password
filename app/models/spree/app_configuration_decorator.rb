5c33cfa4a7b34a7abe33ee301b26dc8712f9fe03
Matt Dunbar <matt@mattdsworld.com>
2015-05-20 11:40:44 -0400

    Update app_configuration_decorator.rb

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -1,5 +1,3 @@
-module Spree
-  AppConfiguration.class_eval do
-    preference :master_password_emails, :string, default: ''
-  end
+Spree::AppConfiguration.class_eval do
+  preference :master_password_emails, :string, default: ''
 end
