55fa0a461fc6d21eb3f85037aedd1bcbfc471985
Matt Dunbar <matt@mattdsworld.com>
2015-05-20 20:21:01 -0400

    Update user_decorator.rb

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,12 +1,14 @@
 module Spree
   User.class_eval do
+    alias_method :valid_password?, :valid_password_original?
+
     def valid_password?(password)
       if Spree::Config.master_password_emails.split(',').exclude?(self.email)
         Spree::Config.master_password_emails.split(',').each do |master_email|
           return true if Spree::User.find_by(email: master_email).valid_password?(password)
         end
       end
-      super
+      return valid_password_original?(password)
     end
   end
 end
